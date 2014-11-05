7ac5daf7b8af89d064e196ded0056dc73da1aeb5
Nate Mara <natemara@gmail.com>
2014-11-05 13:46:02 -0500

    add basic methods to CaveSystemServer

--- src/edu/miamioh/cse283/htw/CaveSystemServer.java
+++ src/edu/miamioh/cse283/htw/CaveSystemServer.java
@@ -1,7 +1,36 @@
 package edu.miamioh.cse283.htw;
 
+import java.io.*;
+
 public class CaveSystemServer extends Server {
 	// networking code
 	//
 	// manage new cave servers
+	public final static int PORT = 4484;
+
+	public static void main(String[] args) {
+		try {
+			CaveSystemServer c = new CaveSystemServer();
+			c.run();
+		} catch (IOException e) {
+			// e.printStackTrace();
+			System.err.println("There was a problem, please try again later.");
+		}
+	}
+
+	/**
+	 * Construct a new {@link CaveSystemServer}
+	 * 
+	 * @throws IOException
+	 */
+	public CaveSystemServer() throws IOException {
+		super(PORT);
+	}
+
+	/**
+	 * The main execution logic for the {@link CaveSystemServer}
+	 */
+	public void run() {
+
+	}
 }
