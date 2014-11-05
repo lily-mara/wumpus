a2cf289e88b06adfaea4414eb44cc4e8d6ac8470
Nate Mara <natemara@gmail.com>
2014-11-05 13:34:43 -0500

    add basic connection method stubs to Client

--- src/edu/miamioh/cse283/htw/Client.java
+++ src/edu/miamioh/cse283/htw/Client.java
@@ -1,5 +1,7 @@
 package edu.miamioh.cse283.htw;
 
+import java.net.*;
+
 public class Client {
 	// networking code
 	//
@@ -7,4 +9,41 @@ public class Client {
 	// handoff
 	// send actions to server
 	// receive messages from server
+
+	public static void main(String[] args) {
+		Client c = new Client();
+		c.run(args);
+	}
+
+	/**
+	 * plays the game
+	 * 
+	 * @param args
+	 *            holds the IP address and port number for a CaveSystemServer
+	 */
+	public void run(String[] args) {
+
+	}
+
+	/**
+	 * connects to a given server
+	 * 
+	 * @param address
+	 *            the address of the server
+	 * @param port
+	 *            the port number of the server
+	 */
+	public void connect(InetAddress address, int port) {
+
+	}
+
+	/**
+	 * transfers this client to a new server
+	 * 
+	 * @param address
+	 * @param port
+	 */
+	public void handoff(InetAddress address, int port) {
+
+	}
 }
