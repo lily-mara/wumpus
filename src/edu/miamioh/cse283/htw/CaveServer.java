c061b1790bd8277bf5d0685651dd2f6a562e72cd
Nate Mara <natemara@gmail.com>
2014-11-05 13:51:19 -0500

    add some basic methods to CaveServer

--- src/edu/miamioh/cse283/htw/CaveServer.java
+++ src/edu/miamioh/cse283/htw/CaveServer.java
@@ -1,9 +1,29 @@
 package edu.miamioh.cse283.htw;
 
+import java.io.*;
+
 public class CaveServer extends Server {
 	// networking code
-	// 
+	//
 	// send cave description to client
 	// client sends actions to server
 	// (play the game)
+	public final static int PORT = 4484;
+
+	public static void main(String[] args) {
+		try {
+			CaveServer c = new CaveServer();
+			c.run();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("There was a problem, please try again later.");
+		}
+	}
+
+	public CaveServer() throws IOException {
+		super(PORT);
+	}
+
+	public void run() {
+	}
 }
