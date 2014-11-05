321f0be827d2255615919dc926d16241a33512ed
Nate Mara <natemara@gmail.com>
2014-11-05 13:38:32 -0500

    improve comments in Client

--- src/edu/miamioh/cse283/htw/Client.java
+++ src/edu/miamioh/cse283/htw/Client.java
@@ -19,7 +19,8 @@ public class Client {
 	 * plays the game
 	 * 
 	 * @param args
-	 *            holds the IP address and port number for a CaveSystemServer
+	 *            holds the IP address and port number for a
+	 *            {@link CaveSystemServer}
 	 */
 	public void run(String[] args) {
 
@@ -29,19 +30,21 @@ public class Client {
 	 * connects to a given server
 	 * 
 	 * @param address
-	 *            the address of the server
+	 *            the address of the {@link Server}
 	 * @param port
-	 *            the port number of the server
+	 *            the port number of the {@link Server}
 	 */
 	public void connect(InetAddress address, int port) {
 
 	}
 
 	/**
-	 * transfers this client to a new server
+	 * transfers this client to a new {@link Server}
 	 * 
 	 * @param address
+	 *            the address of the {@link Server}
 	 * @param port
+	 *            the port number of the {@link Server}
 	 */
 	public void handoff(InetAddress address, int port) {
 
