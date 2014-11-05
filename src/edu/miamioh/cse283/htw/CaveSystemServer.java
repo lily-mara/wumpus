93ed9c7aeb443514454ea27c0b4077c3934d0c35
Nate Mara <natemara@gmail.com>
2014-11-05 12:59:50 -0500

    remove comments from CaveSystemServer

--- src/edu/miamioh/cse283/htw/CaveSystemServer.java
+++ src/edu/miamioh/cse283/htw/CaveSystemServer.java
@@ -3,8 +3,5 @@ package edu.miamioh.cse283.htw;
 public class CaveSystemServer extends Server {
 	// networking code
 	//
-	// receive connections from client
-	// handoff clients to other servers
-	//
 	// manage new cave servers
 }
