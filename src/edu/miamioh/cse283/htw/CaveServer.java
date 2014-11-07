225bf64efe37345f26919e3c193fbd71f710481b
Nate Mara <natemara@gmail.com>
2014-11-07 12:24:36 -0500

    fix compile errors in CaveServer

--- src/edu/miamioh/cse283/htw/CaveServer.java
+++ src/edu/miamioh/cse283/htw/CaveServer.java
@@ -2,7 +2,7 @@ package edu.miamioh.cse283.htw;
 
 import java.io.*;
 
-public class CaveServer extends Server {
+public class CaveServer {
 	// networking code
 	//
 	// send cave description to client
@@ -11,17 +11,8 @@ public class CaveServer extends Server {
 	public final static int PORT = 4484;
 
 	public static void main(String[] args) {
-		try {
-			CaveServer c = new CaveServer();
-			c.run();
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.err.println("There was a problem, please try again later.");
-		}
-	}
-
-	public CaveServer() throws IOException {
-		super(PORT);
+		CaveServer c = new CaveServer();
+		c.run();
 	}
 
 	public void run() {
