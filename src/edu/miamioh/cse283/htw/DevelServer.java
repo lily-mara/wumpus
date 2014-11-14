6c3dff3dee83b78f422cd778e07ca928d3df9ae8
Nate Mara <natemara@gmail.com>
2014-11-14 12:16:51 -0500

    fix development server

--- src/edu/miamioh/cse283/htw/DevelServer.java
+++ src/edu/miamioh/cse283/htw/DevelServer.java
@@ -2,12 +2,47 @@ package edu.miamioh.cse283.htw;
 
 public class DevelServer {
 	public static void main(String[] args) throws Exception {
-		String[] caveSystemServerArgs = { "1234" };
-		String[] caveServerArgs = { "localhost", "1234", "2000" };
-		String[] clientArgs = { "localhost", "1234" };
+		Object lock = new Object();
 
-		CaveSystemServer.main(caveSystemServerArgs);
-		CaveServer.main(caveServerArgs);
-		Client.main(clientArgs);
+		synchronized (lock) {
+			new Thread(new CaveSystemServerDevel()).start();
+			lock.wait(500);
+			new Thread(new CaveServerDevel()).start();
+			lock.wait(500);
+			new Thread(new ClientDevel()).start();
+		}
+	}
+
+	public static class CaveSystemServerDevel implements Runnable {
+		public void run() {
+			String[] caveSystemServerArgs = { "1234" };
+			try {
+				CaveSystemServer.main(caveSystemServerArgs);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static class CaveServerDevel implements Runnable {
+		public void run() {
+			String[] caveServerArgs = { "localhost", "1234", "2000" };
+			try {
+				CaveServer.main(caveServerArgs);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static class ClientDevel implements Runnable {
+		public void run() {
+			String[] clientArgs = { "localhost", "1234" };
+			try {
+				Client.main(clientArgs);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
