6d58a230ab8ce0593d0de819aeaa467c8e151bcb
Nate Mara <natemara@gmail.com>
2014-11-14 13:21:05 -0500

    add comments and default command-line arguments to client class

--- src/edu/miamioh/cse283/htw/Client.java
+++ src/edu/miamioh/cse283/htw/Client.java
@@ -4,6 +4,12 @@ import java.net.*;
 
 /**
  * Client (player) for the Hunt the Wumpus game.
+ * 
+ * The Client class takes the following command-line parameters:
+ * 
+ * <Hostname of CaveSystemServer> <port number of CaveSystemServer>
+ * 
+ * E.g., "localhost 1234"
  *
  */
 public class Client {
@@ -59,8 +65,15 @@ public class Client {
 	 *            client should connect to.
 	 */
 	public static void main(String[] args) throws Exception {
-		CaveProxy cave = new CaveProxy(new Socket(
-				InetAddress.getByName(args[0]), Integer.parseInt(args[1])));
+		InetAddress addr = InetAddress.getByName("localhost");
+		int cavePortBase = 1234;
+
+		if (args.length > 0) {
+			addr = InetAddress.getByName(args[0]);
+			cavePortBase = Integer.parseInt(args[1]);
+		}
+
+		CaveProxy cave = new CaveProxy(new Socket(addr, cavePortBase));
 		Client c = new Client(cave);
 		c.run();
 	}
